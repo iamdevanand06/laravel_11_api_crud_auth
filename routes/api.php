c767e93f18cfab0bccecf2c3ad7dbd81bc15defa
iamdevanand06 <devanand338@gmail.com>
2024-10-12 18:28:46 +0530

    first commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+
+
+// Route::post('register', [RegisterController::class, 'register']);
+// Route::post('login', [RegisterController::class, 'login']);
+Route::resource('products', ProductController::class);
+
+Route::middleware('auth:api')->group( function () {
+    // Route::resource('products', ProductController::class);
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
