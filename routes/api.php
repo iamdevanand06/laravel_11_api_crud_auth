53d723b67829a1fe3afcb1054ec33cc181ad8dac
iamdevanand06 <devanand338@gmail.com>
2024-10-12 20:17:36 +0530

    route removed unwanted

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\API\ProductController;
 
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
-// Route::resource('products', ProductController::class);
 
 Route::middleware('auth:api')->group( function () {
     Route::resource('products', ProductController::class);
